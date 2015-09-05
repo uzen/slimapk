df789b2b524be68967043c5bfe472e5f40431d88
uzen <uzen134@gmail.com>
2015-09-06 00:50:09 +0500

    modified test loop parameters

--- com/uzen/slimapk/App.java
+++ com/uzen/slimapk/App.java
@@ -9,64 +9,64 @@ class App {
     public static void main(String[] args) throws IOException {
         String[] path = new String[2];
         ApkOptions opt = parseArgs(args, path);
+                			System.out.println(path[0]);
+        if(opt != null && path[0] != null) 
         try (SlimApk apk = new SlimApk(path[0], path[1], opt)) {
-        		System.out.printf("Input file(directory): %s\nOutput directory: %s\nArchitecture: %s\n",
-            path[0], path[1], opt.getType());
-            System.out.println("#SlimApk");
+        		System.out.println("#SlimApk\n" + apk.getConfig());
             apk.parseDirectories();
-            System.out.println("File search completed!");
         }
     }
     
     private static ApkOptions parseArgs(final String[] args, String[] path) {
-        if (args.length < 2) SlimApk.debug(getSupport());
+    	
+        if (args.length < 1) {
+        		System.out.println(getSupport());
+        		return null;
+        }
 
         String type = "arm", pattern = null;
-        Boolean keepDir = false, speedMode = false, cleanMode = false;
-        try{
-	        for(int i=0; i < args.length; i++){
-	        		if(!args[i].startsWith("-")) continue; 
-	         	switch (args[i]){
-	         		case "-i": case "--input":
-	                   path[0] = args[++i];
-	                   break;
-	         		case "-o": case "--output":
-	                   path[1] = args[++i];
-	                   break;
-	         		case "-p": case "--pattern":
-	                   pattern = args[++i];
-	                   break;
-	               case "-a32": case "--arm":
-	                   type = "arm";
-	                   break;
-	               case "-a64": case "--arm64":
-	                   type = "arm64";
-	                   break;
-	               case "-x86": case "--x86":
-	                   type = "x86";
-	                   break;
-	               case "-k": case "--keep-dir":
-	                   keepDir = true;
-	                   break;
-	               case "-s": case "--speed-mode":
-	                   speedMode = true;
-	                   break;
-	               case "-h": case "--help":
-	                   SlimApk.debug(getSupport());
-	               default:
-	                   SlimApk.debug("Invalid arguments");
-	         	}   
-	        }
-        } catch (ArrayIndexOutOfBoundsException e) {
-            SlimApk.debug("You did not specify all the arguments");
+        Boolean keepDir = false, cleanMode = false;
+        
+        for(int i=0; i < args.length; i++){
+        		if(!args[i].startsWith("-")){
+        			if(path[0] == null){
+        				path[0] = args[i];
+        			} else if(path[1] == null)
+        				path[1] = args[i];
+        			continue;
+        		}
+        		
+        		if(args[i].startsWith("-p=")){
+        			pattern = args[i].replaceFirst("-p=","");
+        			continue;
+        		}
+        			 
+         	switch (args[i]){
+               case "-a32": case "--arm":
+                   type = "arm";
+                   break;
+               case "-a64": case "--arm64":
+                   type = "arm64";
+                   break;
+               case "-x86": case "--x86":
+                   type = "x86";
+                   break;
+               case "-k": case "--keep-dir":
+                   keepDir = true;
+                   break;
+               case "-h": case "--help":
+                  System.out.println(getSupport());
+                  return null;
+               default:
+               	System.out.println("Invalid argument: " +args[i]);
+         	}   
         }
         
         //remove after:
-        speedMode = true;  //implementation FileXMLParser
         cleanMode = null;	//(new func.)cleaning whole apk of extra libraries
         /*------------*/
         
-        return new ApkOptions(type, pattern, keepDir, speedMode, cleanMode);
+        return new ApkOptions(type, pattern, keepDir, cleanMode);
     }
     
     private static String getImplementation() {
@@ -80,8 +80,8 @@ class App {
          + " -a32 (--arm) - ARM\n" 
          + " -a64 (--arm64) - ARM 64-bit\n" 
          + " -x86 (--x86) - X86\n" 
-         + " -s (--save-directory) keep the structure the f/s\n" 
-         + " -p (--pattern) [\"template for the file name\"]\n" 
+         + " -k (--keep-directory) keep the structure the f/s\n" 
+         + " -p [-p=\"template for the file name\"]\n" 
          + " -h (--help) - help\n" 
          + "\nver. " + getImplementation() + "\n";
     }
