51b38b45bead93dfbc05b8764602b3e9ed953c51
uzen <uzen134@gmail.com>
2015-09-23 01:38:56 +0500

    add option: information about the package

--- java/com/uzen/slimapk/App.java
+++ java/com/uzen/slimapk/App.java
@@ -8,28 +8,30 @@ class App {
 
 	public static void main(String[] args) throws IOException {
 		String[] path = new String[2];
-		ApkOptions opt = parseArgs(args, path);
-		if (path[0] == null || opt == null) {
+		ApkOptions options = parseArgs(args, path);
+		if (path[0] == null || options == null) {
 			return;
 		} else if (path[1] == null) {
 			path[1] = System.getProperty("user.dir");
 		}
-		try (SlimApk apk = new SlimApk(path[0], path[1], opt)) {
-			System.out.println("#SlimApk\n" + apk);
-			apk.parse();
+		try (SlimApk apk = new SlimApk(path[0], path[1], options)) {
+			if(options.getPackageInfo()) {
+				apk.info();
+			} else {
+				apk.parse();
+			}
 		}
-		System.out.println("Done.");
 	}
 
 	private static ApkOptions parseArgs(final String[] args, String[] path) {
 
 		if (args.length < 1) {
-			System.out.println(getSupport());
+			System.err.println(getSupport());
 			return null;
 		}
 
 		String type = "arm", pattern = null, pathToFilesList = null;
-		Boolean keepDir = false;
+		Boolean keepDir = false, packageInfo = false;
 
 		for (int i = 0; i < args.length; i++) {
 			if (!args[i].startsWith("-")) {
@@ -62,15 +64,19 @@ class App {
 				case "-k": case "--keep-dir":
 					keepDir = true;
 					break;
-				case "-h": case "--help":
-					System.out.println(getSupport());
-					return null;
+				case "-i": case "--info":
+					packageInfo = true;
+					break;
 				default:
-					System.out.println("Invalid argument: " + args[i]);
+					System.err.println("Invalid argument: " + args[i]);
 			}
 		}
-
-		return new ApkOptions(type, pattern, pathToFilesList, keepDir);
+		
+		ApkOptions options = new ApkOptions(type, pattern, keepDir);
+		options.setFilesList(pathToFilesList);
+		options.setPackageInfo(packageInfo);		
+		
+		return options;
 	}
 
 	private static String getImplementation() {
@@ -79,6 +85,15 @@ class App {
 	}
 
 	private static String getSupport() {
-		return "\nUsage: slimapk [options...] <INPUT_FOLDER/FILE> <OUTPUT_FOLDER>\n\n" + "options:\n" + " -a32 (--arm) - ARM\n" + " -a64 (--arm64) - ARM 64-bit\n" + " -x86 (--x86) - X86\n" + " -k (--keep-directory) keep the structure the f/s\n" + " -p [-p=\"template for the file name\"]\n" + " -l [-l=\"path to save the list of applications\"]\n" + " -h (--help) - help\n" + "\nver. " + getImplementation() + "\n";
+		return "Usage: slimapk [options...] <INPUT_FOLDER/FILE> <OUTPUT_FOLDER>\n" 
+		+ "options:\n" 
+		+ " -a32 (--arm) - ARM\n" 
+		+ " -a64 (--arm64) - ARM 64-bit\n" 
+		+ " -x86 (--x86) - X86\n" 
+		+ " -k (--keep-directory) keep the structure the f/s\n" 
+		+ " -p [-p=\"default template file name\"]\n" 
+		+ " -l [-l=\"path to save the list of applications\"]\n" 
+		+ " -i (--info) get more information about a specific package\n"
+		+ "\nver. " + getImplementation() + "\n";
 	}
 }
